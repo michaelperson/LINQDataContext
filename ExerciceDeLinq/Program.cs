a1e62ddbc20a7b24c529615fab6e5d98d39386c5
Person M <michael.person@cognitic.be>
2022-01-18 14:41:45 +0100

    Add project files.

---
+++ ExerciceDeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LINQDataContext;
+
+namespace ExerciceDeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext dc = new DataContext();
+            Console.WriteLine("test");
+            #region Console.ReadLine()
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
